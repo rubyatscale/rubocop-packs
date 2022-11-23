6b313d8bbba9cf49c3c9c0871ec499591d09cdd2
Alex Evanczuk <alex.evanczuk@gusto.com>
2022-11-23 08:49:37 -0500

    Make test pass

--- lib/rubocop/cop/packs/class_methods_as_public_apis.rb
+++ lib/rubocop/cop/packs/class_methods_as_public_apis.rb
@@ -52,6 +52,9 @@ module RuboCop
           class_is_allowed_to_have_instance_methods = acceptable_parent_classes.include?(parent_class&.const_name)
           return if uses_implicit_static_methods || class_is_allowed_to_have_instance_methods
 
+          is_interface = !module_node.nil? && module_node.descendants.any?{|d| d.is_a?(RuboCop::AST::SendNode) && d.method_name == :interface! }
+          return if is_interface
+
           add_offense(
             node.source_range,
             message: format(
