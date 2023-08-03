bbecbeb60654becb74cf6d724403380eb18a5552
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-08-03 15:02:25 -0400

    Stimpack => packs-rails

--- lib/rubocop/cop/packs/root_namespace_is_pack_name.rb
+++ lib/rubocop/cop/packs/root_namespace_is_pack_name.rb
@@ -8,7 +8,7 @@ module RuboCop
   module Cop
     module Packs
       # This cop helps ensure that each pack exposes one namespace.
-      # Note that this cop doesn't necessarily expect you to be using stimpack (https://github.com/rubyatscale/stimpack),
+      # Note that this cop doesn't necessarily expect you to be using packs-rails (https://github.com/rubyatscale/packs-rails),
       # but it does expect packs to live in the organizational structure as described in the README.md of that gem.
       #
       # This allows packs to opt in and also prevent *other* files from sitting in their namespace.
